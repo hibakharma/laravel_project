b334f29b94bc554b401735353c9ef378b1f7d504
Hiba Kharma <hibakharma@gmail.com>
2023-10-29 18:07:26 +0400

    install them

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,38 @@
+@extends('admin.master')
+
+@section('title')
+    dashboard
+@stop
+
+@section('css')
+
+@stop
+
+@section('content')
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0 text-dark">dashboard </h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">dashboard </li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+
+@endsection
+
+@section('js')
+
+@endsection
